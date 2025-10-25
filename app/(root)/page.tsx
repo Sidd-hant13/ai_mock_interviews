11e00b8a1a038e34b17dd7135ecc4586ce16842f
Siddhant <siddhant.prasad8@gmail.com>
2025-10-26 03:05:35 +0530

    Implement interview session

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import { dummyInterviews } from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
 import {
-  getCurrentUser,
+  getCurrentUser
+} from "@/lib/actions/auth.action";
+import {
   getInterviewsByUserId,
   getLatestInterviews
-} from "@/lib/actions/auth.action";
+} from "@/lib/actions/general.action";
 
 const page = async () => {
   const user = await getCurrentUser();
